82eabff88059b8ee507d0cbef241778b91cea906
Robinlop64 <robin.rubiks@gmail.com>
2024-07-03 09:04:42 -0700

    v2p

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log("Corriendo we")
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
